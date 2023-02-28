26db572bc699f6eb0b0c56ad89e8759223d484b7
Yuanchen <sgyliu78@liverpool.ac.uk>
2023-02-28 02:39:56 +0000

    add 3dunet model

---
+++ examples/3d_unet/3dunet_joey.py
@@ -0,0 +1,148 @@
+from itertools import product
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import joey
+from torchsummary import summary
+
+# class Downward(nn.Module):
+
+
+class test_pytorch(nn.Module):
+    def __init__(self, in_channel = None, filter = 8) -> None:
+        super(test_pytorch, self).__init__()
+        self.in_channel = in_channel
+        self.filter = filter
+
+        # Downward block
+        self.DB1_CB = self.CB(in_channel, self.filter, stride=1, k_size=3)
+        self.DB1 = self.DB(self.filter, self.filter, stride=1)
+        self.DB2_CB = self.CB(self.filter, self.filter*2, stride=2, k_size=3)
+        self.DB2 = self.DB(self.filter*2, self.filter*2, stride=1)
+        self.DB3_CB = self.CB(self.filter*2, self.filter*4, stride=2, k_size=3)
+        self.DB3 = self.DB(self.filter*4, self.filter*4, stride=1)
+        self.DB4_CB = self.CB(self.filter*4, self.filter*8, stride=2, k_size=3)
+        self.DB4 = self.DB(self.filter*8, self.filter*8, stride=1)
+        self.DB5_CB = self.CB(self.filter*8, self.filter*16, stride=2, k_size=3)
+        self.DB5 = self.DB(self.filter*16, self.filter*16, stride=1)
+
+
+
+        # Upward Block
+        self.UB1_U3_CB = self.UB_U3_CB(self.filter*16, self.filter*8)
+        self.UB1_CB_CB = self.UB_CB_CB(self.filter*16, self.filter*8)
+        self.UB2_U3_CB = self.UB_U3_CB(self.filter*8, self.filter*4)
+        self.UB2_CB_CB = self.UB_CB_CB(self.filter*8, self.filter*4)
+        self.UB3_U3_CB = self.UB_U3_CB(self.filter*4, self.filter*2)
+        self.UB3_CB_CB = self.UB_CB_CB(self.filter*4, self.filter*2)
+        self.UB4_U3_CB = self.UB_U3_CB(self.filter*2, self.filter)
+        self.UB4_CB_CB = self.UB_CB_CB(self.filter*2, self.filter)
+
+
+    def CB(self, in_channel, filter, stride, k_size, padding=1):
+        return nn.Sequential(joey.Conv3D(in_channel, filter, kernel_size=(3,3,3), stride=stride, padding=padding, bias=False),
+                             joey.InstanceNorm3d(filter),
+                             joey.LeakyReLU())
+
+    def C3(self, in_channel):
+        return joey.Conv3D(in_channel, 3, kernel_size=1, stride=1, padding=0, bias=False)
+    
+    def DB(self, in_channel, filter, stride):
+        return nn.Sequential(self.CB(in_channel, filter,stride),
+                             nn.Dropout3d(),
+                             self.CB(filter, filter))
+    
+    def U3(self, in_channel):
+        return joey.Upsample(in_channel, scale_factor=2, mode='nearest')
+
+
+    
+    def UB_U3_CB(self, in_channel, filter):
+        return nn.Sequential(self.U3(in_channel),
+                             self.CB(in_channel, filter))
+
+    def UB_CB_CB(self, in_channel, filter):
+        return nn.Sequential(self.CB(in_channel, filter),
+                             self.CB(filter, filter, kernel_size=1,padding=0))
+
+    def forward(self, input):
+
+        #Level 1 DB(16,1)
+        out = self.DB1_CB(input)
+        tempout1 = out
+        out = self.DB1(out)
+        out += tempout1
+        DB1 = out
+
+        #Level 2 DB(32,2)
+        out = self.DB2_CB(out)
+        tempout2 = out
+        out = self.DB2(out)
+        out += tempout2
+        DB2 = out
+
+        #Level 3 DB(64,2)
+        out = self.DB3_CB(out)
+        tempout3 = out
+        out = self.DB3(out)
+        out += tempout3
+        DB3 = out
+
+        #Level 4 DB(128,2)
+        out = self.DB4_CB(out)
+        tempout4 = out
+        out = self.DB4(out)
+        out += tempout4
+        DB4 = out
+        
+        #Level 5 DB(256,2)
+        out = self.DB5_CB(out)
+        tempout5 = out
+        out = self.DB5(out)
+        out += tempout5
+        
+        #Level 1 UP(128)
+        out = self.UB1_U3_CB(out)
+        out = torch.cat([out, DB4], dim=1)
+        out = self.UB1_CB_CB(out)
+
+        #Level 2 UP(64)
+        out = self.UB2_U3_CB(out)
+        out = torch.cat([out, DB3], dim=1)
+        out = self.UB2_CB_CB(out)
+
+        #upscale 1
+        upscale1 = self.C3(out)
+        upscale1 = self.U3(upscale1)
+
+        #Level 3 UP(32)
+        out = self.UB3_U3_CB(out)
+        out = torch.cat([out, DB2], dim=1)
+        out = self.UB3_CB_CB(out)
+
+        #upscale 2
+        upscale2 = self.C3(out)
+        upscale2 += upscale1
+        upscale2 = self.U3(upscale2) 
+
+        #Level 4 UP(16)
+        out = self.UB4_U3_CB(out)
+        out = torch.cat([out, DB1], dim=1)
+        out = self.UB4_CB_CB(out)
+        
+        #upscale 3
+        out = self.C3(out)
+        out += upscale2
+
+        #activation
+        out = nn.Sigmoid(out)
+
+        return out
+
+
+input = torch.rand(5,4)
+
+m = test_pytorch(8)
+print(m)
---
+++ examples/3d_unet/3dunet_test.py
@@ -0,0 +1,172 @@
+from itertools import product
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torchsummary import summary
+from torch.autograd import Variable
+
+# class Downward(nn.Module):
+
+classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+class test_pytorch(nn.Module):
+    def __init__(self, in_channel = 4, filter = 16) -> None:
+        super(test_pytorch, self).__init__()
+        self.in_channel = in_channel
+        self.filter = filter
+
+        # Downward block
+        self.DB1_CB = self.CB(in_channel=in_channel, filter=self.filter, stride=1, kernel_size=3)
+        self.DB1 = self.DB(in_channel=self.filter, filter=self.filter, stride=1)
+        self.DB2_CB = self.CB(in_channel=self.filter, filter=self.filter*2, stride=2, kernel_size=3)
+        self.DB2 = self.DB(in_channel=self.filter*2, filter=self.filter*2, stride=1)
+        self.DB3_CB = self.CB(in_channel=self.filter*2, filter=self.filter*4, stride=2, kernel_size=3)
+        self.DB3 = self.DB(in_channel=self.filter*4, filter=self.filter*4, stride=1)
+        self.DB4_CB = self.CB(in_channel=self.filter*4, filter=self.filter*8, stride=2, kernel_size=3)
+        self.DB4 = self.DB(in_channel=self.filter*8, filter=self.filter*8, stride=1)
+        self.DB5_CB = self.CB(in_channel=self.filter*8, filter=self.filter*16, stride=2, kernel_size=3)
+        self.DB5 = self.DB(in_channel=self.filter*16, filter=self.filter*16, stride=1)
+
+
+
+        # Upward Block
+        self.UB1_U3_CB = self.UB_U3_CB(self.filter*16, self.filter*8)
+        self.UB1_CB_CB = self.UB_CB_CB(self.filter*16, self.filter*8)
+        self.UB2_U3_CB = self.UB_U3_CB(self.filter*8, self.filter*4)
+        self.UB2_CB_CB = self.UB_CB_CB(self.filter*8, self.filter*4)
+        self.UB3_U3_CB = self.UB_U3_CB(self.filter*4, self.filter*2)
+        self.UB3_CB_CB = self.UB_CB_CB(self.filter*4, self.filter*2)
+        self.UB4_U3_CB = self.UB_U3_CB(self.filter*2, self.filter)
+        self.UB4_CB_CB = self.UB_CB_CB(self.filter*2, self.filter)
+        self.C3_1 = self.C3(self.filter*4)
+        self.C3_2 = self.C3(self.filter*2)
+        self.C3_3 = self.C3(self.filter)
+        self.U3_1 = self.U3()
+        self.U3_2 = self.U3()
+        self.sigmoid = nn.Sigmoid()
+
+    def CB(self, in_channel, filter, stride=1, kernel_size=3,padding=1):
+        return nn.Sequential(nn.Conv3d(in_channel, filter, kernel_size=kernel_size, stride=stride, padding=padding, bias=False),
+                             nn.InstanceNorm3d(filter),
+                             nn.LeakyReLU())
+
+    def C3(self, in_channel,filter=3):
+        return nn.Conv3d(in_channel, filter, kernel_size=1, stride=1, padding=0, bias=False)
+    
+    def DB(self, in_channel, filter, stride):
+        return nn.Sequential(self.CB(in_channel, filter, stride),
+                             nn.Dropout3d(),
+                             self.CB(filter, filter))
+    
+    def U3(self):
+        return nn.Upsample(scale_factor=2, mode='nearest')
+
+
+    
+    def UB_U3_CB(self, in_channel, filter):
+        return nn.Sequential(self.U3(),
+                             self.CB(in_channel, filter))
+
+    def UB_CB_CB(self, in_channel, filter):
+        return nn.Sequential(self.CB(in_channel, filter),
+                             self.CB(filter, filter, kernel_size=1,padding=0))
+
+    def forward(self, input):
+
+        #Level 1 DB(16,1)
+        out = self.DB1_CB(input)
+        tempout1 = out
+        out = self.DB1(out)
+        out += tempout1
+        DB1 = out
+
+        #Level 2 DB(32,2)
+        out = self.DB2_CB(out)
+        tempout2 = out
+        out = self.DB2(out)
+        out += tempout2
+        DB2 = out
+
+        #Level 3 DB(64,2)
+        out = self.DB3_CB(out)
+        tempout3 = out
+        out = self.DB3(out)
+        out += tempout3
+        DB3 = out
+
+        #Level 4 DB(128,2)
+        out = self.DB4_CB(out)
+        tempout4 = out
+        out = self.DB4(out)
+        out += tempout4
+        DB4 = out
+        
+        #Level 5 DB(256,2)
+        out = self.DB5_CB(out)
+        tempout5 = out
+        out = self.DB5(out)
+        out += tempout5
+        
+        #Level 1 UP(128)
+        out = self.UB1_U3_CB(out)
+        out = torch.cat([out, DB4], dim=1)
+        out = self.UB1_CB_CB(out)
+
+        #Level 2 UP(64)
+        out = self.UB2_U3_CB(out)
+        out = torch.cat([out, DB3], dim=1)
+        out = self.UB2_CB_CB(out)
+
+        #upscale 1
+        upscale1 = self.C3_1(out)
+        upscale1 = self.U3_1(upscale1)
+
+        #Level 3 UP(32)
+        out = self.UB3_U3_CB(out)
+        out = torch.cat([out, DB2], dim=1)
+        out = self.UB3_CB_CB(out)
+
+        #upscale 2
+        upscale2 = self.C3_2(out)
+        upscale2 += upscale1
+        upscale2 = self.U3_2(upscale2) 
+
+        #Level 4 UP(16)
+        out = self.UB4_U3_CB(out)
+        out = torch.cat([out, DB1], dim=1)
+        out = self.UB4_CB_CB(out)
+        
+        #upscale 3
+        out = self.C3_3(out)
+        out += upscale2
+
+        #activation
+        out = self.sigmoid(out)
+
+        return out
+
+transform = transforms.Compose(
+    [transforms.Resize((32,32)),
+     transforms.ToTensor(),
+     transforms.Normalize(0.5,0.5)])
+trainset = torchvision.datasets.MNIST(root='./minst', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=False, num_workers=2)
+
+# class Train:
+#     def __init__(self) -> None:
+#         self.args = args
+#         self.model = model
+
+# dataiter = iter(trainloader)
+# images, labels = next(dataiter)
+# input = images.double().numpy()
+# unet_3d = test_pytorch(input, 8)
+# unet_3d.double()
+input = Variable(torch.randn(16, 4, 64, 64, 64))  # b,c,z,h,w
+m = test_pytorch(in_channel = 4, filter = 16)
+output = m(input)
+print(output.shape)
